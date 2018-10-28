bc16744826ee170f8207a59df3384e03e10a2e8e
Nikheel <p.nikheel@gmail.com>
2018-10-28 16:26:38 -0700

    Added Few Files

---
+++ rigging_ui.py
@@ -0,0 +1,15 @@
+#GENERAL INFO
+_version = 'V001'
+_author =  'Nikheel patel'
+_email  =  'spark.visualartist@gmail.com'
+_last_modify = '10/28/2018'
+
+#ALL PATH
+
+
+
+
+_pyqt_path = 'C:/Python27/Lib/site-packages'
+_pyqt5_path = 'C:/Python35/Lib/site-packages'
+#_lib_path = 'C:/Users/ravig/Desktop/Nikheel/lib/'
+_lib_path = 'G:/Script/lib/'
